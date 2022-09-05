13a787bb016e36b6efed8923c04092ec9577fd49
hyeonho <6190606@naver.com>
2022-09-05 21:06:50 +0900

    update-config-value

--- cards/src/main/java/com/hyunho9877/cards/CardsApplication.java
+++ cards/src/main/java/com/hyunho9877/cards/CardsApplication.java
@@ -2,7 +2,9 @@ package com.hyunho9877.cards;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 
+@RefreshScope
 @SpringBootApplication
 public class CardsApplication {
 
