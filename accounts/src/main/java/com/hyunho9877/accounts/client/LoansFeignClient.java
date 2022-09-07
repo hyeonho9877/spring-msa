b24d5c846f6120eb2d0aff18217ae88674188d02
hyeonho <6190606@naver.com>
2022-09-07 22:03:37 +0900

    gateway-with-trace-id

--- accounts/src/main/java/com/hyunho9877/accounts/client/LoansFeignClient.java
+++ accounts/src/main/java/com/hyunho9877/accounts/client/LoansFeignClient.java
@@ -5,14 +5,14 @@ import com.hyunho9877.accounts.model.Loans;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
-import javax.ws.rs.core.Variant;
 import java.util.List;
 
 @FeignClient("loans")
 public interface LoansFeignClient {
     @RequestMapping(method = RequestMethod.POST, value = "myLoans", consumes = MediaType.APPLICATION_JSON_VALUE)
-    List<Loans> getLoansDetail(@RequestBody Customer customer);
+    List<Loans> getLoansDetail(@RequestHeader(name = "eazybank-correlation-id") String correlationId, @RequestBody Customer customer);
 }
--- cards/src/main/java/com/hyunho9877/cards/controller/CardsController.java
+++ cards/src/main/java/com/hyunho9877/cards/controller/CardsController.java
@@ -7,10 +7,7 @@ import com.hyunho9877.cards.model.Properties;
 import com.hyunho9877.cards.repository.CardsRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -22,7 +19,7 @@ public class CardsController {
     private final CardsServiceConfig cardsConfig;
 
     @PostMapping("/myCards")
-    public List<Cards> getCardDetails(@RequestBody Customer customer) {
+    public List<Cards> getCardDetails(@RequestHeader("eazybank-correlation-id") String correlationId, @RequestBody Customer customer) {
         return cardsRepository.findByCustomerId(customer.getCustomerId());
     }
 
