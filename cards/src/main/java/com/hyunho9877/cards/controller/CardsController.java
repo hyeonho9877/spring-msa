4cd9aa1e48b1af976926d54f7e4088603e83a1db
hyeonho <6190606@naver.com>
2022-09-08 16:45:57 +0900

    spring-cloud-slueth

--- cards/src/main/java/com/hyunho9877/cards/controller/CardsController.java
+++ cards/src/main/java/com/hyunho9877/cards/controller/CardsController.java
@@ -6,6 +6,8 @@ import com.hyunho9877.cards.model.Customer;
 import com.hyunho9877.cards.model.Properties;
 import com.hyunho9877.cards.repository.CardsRepository;
 import lombok.RequiredArgsConstructor;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -15,12 +17,16 @@ import java.util.List;
 @RequiredArgsConstructor
 public class CardsController {
 
+    private static final Logger logger = LoggerFactory.getLogger(CardsController.class);
     private final CardsRepository cardsRepository;
     private final CardsServiceConfig cardsConfig;
 
     @PostMapping("/myCards")
     public List<Cards> getCardDetails(@RequestHeader("eazybank-correlation-id") String correlationId, @RequestBody Customer customer) {
-        return cardsRepository.findByCustomerId(customer.getCustomerId());
+        logger.info("CardsController.getCardDetails started");
+        List<Cards> cards = cardsRepository.findByCustomerId(customer.getCustomerId());
+        logger.info("CardsController.getCardDetails ended");
+        return cards;
     }
 
     @GetMapping("/cards/properties")
